62e4a0a79aa9932057b372e9110782acd5cd3dd2
ryaanmohideen <124945546+ryaanmohideen@users.noreply.github.com>
2024-01-08 18:57:41 -0500

    final changes to game

---
+++ street fighter-like game/NE111_Final_Game_Project.py
@@ -0,0 +1,155 @@
+# Initialization - Zain Khalil
+import pygame  
+from pygame import mixer
+from fighter import Fighter
+
+mixer.init()
+pygame.init()
+
+
+#Creating Game Window - Ryan Rahman
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("DBZ Fighter")
+
+#set framerate - Owen Gibbs
+clock = pygame.time.Clock()
+FPS = 60
+
+#define colours - Owen Gibbs
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+
+#define game variables - Ryaan Mohideen
+intro_count = 3
+last_count_update = pygame.time.get_ticks()
+score = [0,0] ##player scores - [P1, P2]
+round_over = False
+ROUND_OVER_COOLDOWN = 2000
+
+
+#Define Fighter Variables - Ryan Rahman
+GOKU_WIDTH = 150
+GOKU_HEIGHT = 175
+GOKU_SCALE = 1
+GOKU_DATA = [GOKU_WIDTH,GOKU_HEIGHT, GOKU_SCALE]
+VEGETA_WIDTH = 150
+VEGETA_HEIGHT = 137
+VEGETA_SCALE = 1.2
+VEGETA_DATA = [VEGETA_WIDTH,VEGETA_HEIGHT,VEGETA_SCALE]
+
+# Load Music and Sound Effects - Zain Khalil
+pygame.mixer.music.load("assets/audio/Naruto Theme - The Raising Fighting Spirit.mp3")
+pygame.mixer.music.set_volume(0.5) # Volume of music
+pygame.mixer.music.play(-1, 0.0, 5000) # Repitition, Slowly Fades in @ Beginning
+punch1_fx = pygame.mixer.Sound("assets/audio/Dragon Ball Z Punch Sound Effect.wav") # Selected Sound effect for fighter_1
+punch1_fx.set_volume(0.5)
+punch2_fx = pygame.mixer.Sound("assets/audio/Dragon Ball Z Punch Sound Effect.wav") # Selected Sound effect for fighter_2
+punch2_fx.set_volume(0.5)
+
+#Load Background - Ryan Rahman
+bg_image = pygame.image.load("assets/bg_image - instasize.jpg").convert_alpha()
+
+#Load Spritesheets - Ryan Rahman
+goku_sheet = pygame.image.load("assets/Goku/Goku Super Saiyan.png").convert_alpha()
+vegeta_sheet = pygame.image.load("assets/Vegeta/vegeta.png").convert_alpha()
+
+#Define number of steps in each animation - Ryan Rahman
+GOKU_ANIMATION_STEPS = [6,8,4,6,3,8,3,4,6,9,2,4,3]
+VEGETA_ANIMATION_STEPS = [6,6,4,6,3,7,3,4,6,5,2,3,4]
+
+#define font - Ryaan Mohideen
+count_font = pygame.font.Font("assets/pixelated_princess.ttf", 80)
+score_font = pygame.font.Font("assets/pixelated_princess.ttf", 35)
+
+#function for drawing text - Ryaan Mohideen
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x,y))
+    
+#Function to Draw Background - Ryan Rahman
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH,SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0,0))
+
+#function for drawing fighter health bars - Owen Gibbs
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x -2, y -2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
+
+#create two instances of fighter - Zain Khalil
+fighter_1 = Fighter(1, 200, 310,False, GOKU_DATA, goku_sheet,GOKU_ANIMATION_STEPS,punch1_fx)
+fighter_2 = Fighter(2, 700, 310,False, VEGETA_DATA, vegeta_sheet,VEGETA_ANIMATION_STEPS,punch2_fx)
+
+#Game Loop - Ryan Rahman
+run = True
+while run:
+    
+    clock.tick(FPS)
+
+    #Draw Background - Ryan Rahman
+    draw_bg()
+
+    #show player stats - Owen Gibbs
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)
+    ##show scores - Ryaan Mohideen
+    draw_text("P1: "  + str(score[0]), score_font, RED, 20, 60 )
+    draw_text("P2: "  + str(score[1]), score_font, RED, 580, 60 )
+    #update countdown - Ryaan Mohideen
+    if intro_count <= 0:
+        #move fighters - Owen Gibbs
+        fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2, round_over)
+        fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1, round_over)
+    else:
+        #display count timer - Ryaan Mohideen
+        draw_text(str(intro_count), count_font, RED, SCREEN_WIDTH/2, SCREEN_HEIGHT/3)
+        #update count timer - Ryaan Mohideen
+        if (pygame.time.get_ticks() - last_count_update) > 1000:
+            intro_count -= 1 
+            last_count_update = pygame.time.get_ticks()
+    
+    #update fighters - Ryan Rahman
+    fighter_1.update()
+    fighter_2.update()
+
+    #draw fighter - Owen Gibbs
+    fighter_1.draw(screen)
+    fighter_2.draw(screen)
+
+    #check for player defeat - Ryaan Mohideen
+    if round_over == False:
+        if fighter_1.alive == False:
+            score[1]+=1
+            round_over = True
+            round_over_time = pygame.time.get_ticks()
+        elif fighter_2.alive == False:
+            score[0]+=1
+            round_over = True
+            round_over_time = pygame.time.get_ticks()
+    else:
+        #display victory image - Zain Khalil 
+        draw_text("VICTORY!", count_font, RED, 360, 150)
+        if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
+            round_over = False
+            intro_count = 3
+            fighter_1 = Fighter(1, 200, 310,False, GOKU_DATA, goku_sheet,GOKU_ANIMATION_STEPS,punch1_fx)
+            fighter_2 = Fighter(2, 700, 310,False, VEGETA_DATA, vegeta_sheet,VEGETA_ANIMATION_STEPS,punch2_fx)
+    #Event Handler - Ryan Rahman
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    #Update Display - Ryan Rahman
+    pygame.display.update()
+            
+
+#exit pygame - Ryan Rahman
+
+pygame.quit()
---
+++ street fighter-like game/fighter.py
@@ -0,0 +1,197 @@
+import pygame
+
+#Owen Gibbs
+class Fighter():
+    def __init__(self, player, x, y,flip,data,sprite_sheet,animation_steps,sound):
+        self.player = player
+        self.width = data[0]
+        self.height = data[1]
+        self.image_scale = data[2]
+        self.flip = flip
+        self.animation_list = self.load_images(sprite_sheet, animation_steps)
+        self.action = 0 #0: idle #1: run #2: jump #3: attack1 #4: attack2 #5: hit #6: death #7: transform
+        self.frame_index = 0
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.update_time= pygame.time.get_ticks()
+        self.rect = pygame.Rect((x, y, 80, 180))
+        self.vel_y = 0
+        self.running = False    # Zain Khalil
+        self.jump = False
+        self.attacking = False
+        self.attack_type = 0
+        self.attack_cooldown = 0
+        self.attack_sound = sound
+        self.hit = False
+        self.health = 100
+        self.alive = True
+
+
+    def load_images(self,sprite_sheet,animation_steps):
+        #Extract images from spritesheet - Owen Gibbs
+        animation_list = []
+        for y, animation in enumerate(animation_steps):
+            temp_img_list = []
+            for x in range(animation):
+                temp_img = sprite_sheet.subsurface(x * self.width,y * self.height,self.width,self.height)
+                temp_img_list.append(pygame.transform.scale(temp_img,(self.width * self.image_scale, self.height * self.image_scale)))
+            animation_list.append(temp_img_list)
+        return animation_list
+    
+    def move(self, screen_width, screen_height, surface, target, round_over):
+        SPEED = 10
+        GRAVITY = 2
+        dx = 0
+        dy = 0
+        self.running = False
+        self.attack_type = 0
+
+        #get keypresses - Zain Khalil
+        key = pygame.key.get_pressed()
+
+        #can only perfom other actions if not currently attacking
+        #movement
+        if self.attacking == False and self.alive == True and round_over == False:
+            #checks player 1's controls - Ryaan Mohideen
+            if self.player == 1:
+                if key[pygame.K_a]:
+                    dx = -SPEED
+                    self.running = True
+                if key[pygame.K_d]:
+                    dx = SPEED 
+                    self.running = True
+                #jump
+                if key[pygame.K_w] and self.jump == False:
+                    self.vel_y = -30
+                    self.jump = True
+                #attack
+                if key[pygame.K_r] or key[pygame.K_t]:
+                    self.attack(target)
+                    #determine which attack type was used
+                    if key[pygame.K_r]:
+                        self.attack_type = 1
+                    if key[pygame.K_t]:
+                        self.attack_type = 2
+            #checks player 2's controls - Ryaan Mohideen
+            if self.player == 2:
+                if key[pygame.K_LEFT]:
+                    dx = -SPEED
+                    self.running = True
+                if key[pygame.K_RIGHT]:
+                    dx = SPEED 
+                    self.running = True
+                #jump
+                if key[pygame.K_UP] and self.jump == False:
+                    self.vel_y = -30
+                    self.jump = True
+                #attack
+                if key[pygame.K_RETURN] or key[pygame.K_RSHIFT]:
+                    self.attack(target)
+                    #determine which attack type was used
+                    if key[pygame.K_RETURN]:
+                        self.attack_type = 1
+                    if key[pygame.K_RSHIFT]:
+                        self.attack_type = 2
+
+            
+            
+        #apply gravity - Owen Gibbs
+        self.vel_y += GRAVITY
+        dy += self.vel_y
+
+        #ensure player stays on screen - Owen Gibbs
+        if self.rect.left + dx < 0:
+            dx = 0 -self.rect.left
+        if self.rect.right +dx > screen_width:
+            dx = screen_width - self.rect.right
+        if self.rect.bottom + dy > screen_height - 110:
+            self.vel_y = 0
+            self.jump = False
+            dy = screen_height - 110 - self.rect.bottom
+        
+        #ensure plays face eachother - Zain Khalil
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = True
+        else:
+            self.flip = False
+        
+        #Apply attack cooldown - Ryan Rahman
+        if self.attack_cooldown > 0:
+            self.attack_cooldown -= 1
+
+        #update player position - Zain Khalil
+        self.rect.x += dx
+        self.rect.y += dy
+    
+
+    #Handle animation updates - Ryan Rahman
+    def update(self):
+        #Check what action the player is performing - Ryan Rahman
+        if self.health<= 0:
+            self.health = 0
+            self.alive = False
+            self.update_action(11)
+        elif self.hit == True:
+           self.update_action(10) 
+        elif self.attacking == True:
+            if self.attack_type == 1:
+                self.update_action(7)
+            elif self.attack_type == 2:
+                self.update_action(8)
+        elif self.jump == True:
+            self.update_action(5)
+        elif self.running == True:
+            self.update_action(6)
+        else:
+            self.update_action(12)
+        animation_cooldown = 150
+        self.image = self.animation_list[self.action][self.frame_index]
+        #Check if enough time has passed since the last update - Ryan Rahman
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.frame_index += 1
+            self.update_time = pygame.time.get_ticks()
+        #Check if the animation has finished - Ryan Rahman
+        if self.frame_index >= len(self.animation_list[self.action]):
+            #If the player is dead then end the animation - Ryan Rahman
+            if self.alive == False:
+                self.frame_index = len(self.animation_list[self.action]) - 1
+            else:
+                self.frame_index = 0
+                #Check if an attack has been executed - Ryan Rahman
+                if self.action == 7 or self.action == 8:
+                    self.attacking = False
+                    self.attack_cooldown = 50
+                #Check if Damage was taken - Ryan Rahman
+                if self.action == 10:
+                    self.hit = False
+                    #If the player was on the middle of the attack, stop attack - Ryan Rahman
+                    self.attacking = False
+                    self.attack_cooldown = 20
+    #Zain Khalil
+    def attack(self, target):
+        if self.attack_cooldown == 0:    #execute attack
+            self.attacking = True 
+            self.attack_sound.play()
+            attacking_rect = pygame.Rect(self.rect.centerx - 2*self.rect.width * self.flip, self.rect.y, 4 * self.rect.width, self.rect.height)
+            if attacking_rect.colliderect(target.rect):
+                target.health -= 10
+                target.hit = True
+    
+   #Zain Khalil
+    def update_action(self,new_action):
+        #Check if the new action is different to the previous one
+        if new_action  != self.action:
+            self.action = new_action
+            #Update the animation settings
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
+
+
+    #Zain Khalil
+    def draw(self, surface):
+        img = pygame.transform.flip(self.image, self.flip,False)
+        surface.blit(img,(self.rect.x,self.rect.y))
+
+
+
+
+
